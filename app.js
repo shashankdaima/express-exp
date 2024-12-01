367c357a981b4f4bb19f29378447ac29a1acf05f
Shashank Daima <daimashashank10@gmail.com>
2024-12-01 18:22:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const os = require('os');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.get('/health', (req, res) => {
   const health = {
